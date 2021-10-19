e8e86c415ffb5c821fada85f45196877ff391ca9
Ayan Pahwa <ayan@balena.io>
2021-10-20 02:38:14 +0530

    update dependencies

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y gstreamer1.0-tools \
+sudo apt-get install -y pulseaudio
+                        gstreamer1.0-tools \
                         gstreamer1.0-plugins-base \
                         gstreamer1.0-plugins-good \
                         gstreamer1.0-pulseaudio
